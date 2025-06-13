aafb8096d15473a14e4be7ffc441d4a60f25136e
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-06-13 23:08:43 +0530

    add: app

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from model import *
 import os.path
 import getpass
 
-## Set up the API key
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
 
